edfc5c1ededfa2fea0e849a4fece391e747bbf95
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2023-01-26 01:06:52 +0200

    UI updates - adds color package so we can log using nice colors - updates base to return number/string - removes usage of ux package and dead code

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,5 +3,5 @@ import {readFileSync} from 'node:fs'
 import {join} from 'node:path'
 
 export const getTestPuzzleInput = (day: number): string => {
-  return readFileSync(join(__dirname, `day${day}.txt`), {encoding: 'utf8'})
+  return readFileSync(join(__dirname, `day${day}`), {encoding: 'utf8'})
 }
