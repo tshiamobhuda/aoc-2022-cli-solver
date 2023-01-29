1ac22d5c52d1f76c76b6b176df5713ca3dec80ec
Tshiamo <tshiamobhuda@gmail.com>
2023-01-29 18:39:29 +0200

    Adds solution for day 3 - adds getter for input to return it as array

--- src/service/solution-map.ts
+++ src/service/solution-map.ts
@@ -1,8 +1,10 @@
 import {BaseSolution} from '../solutions/base'
 import {Day1} from '../solutions/day1'
 import {Day2} from '../solutions/day2'
+import {Day3} from '../solutions/day3'
 
 export const aocSolutions = new Map <string, typeof BaseSolution>([
   ['1', Day1],
   ['2', Day2],
+  ['3', Day3],
 ])
